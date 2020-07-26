1a58bcba914ed9528927e48a41b8b31bdae5798e
Kossie Coder <kossie.coder@gmail.com>
2020-07-26 17:16:43 +1000

    03

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function App() {
+
+  const onSubmit = () => {
+    alert('submitted');
+  };
+
+  const onKeyUp = (event) => {
+    if (event.keyCode === 13) {
+      onSubmit();
+    }
+  }
+
   return (
     <div className="App">
-      Kossie Coder
+      <input onKeyUp={onKeyUp} />
+      <button onClick={onSubmit}>Submit</button>
     </div>
   );
 }
