88b2634e1966ff5041758a96ffc915a1bdeda290
Kossie Coder <kossie.coder@gmail.com>
2020-07-26 17:57:30 +1000

    04

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-
+  const [text, setText] = useState('Kossie');
   const onSubmit = () => {
     alert('submitted');
   };
@@ -12,10 +12,25 @@ function App() {
     }
   }
 
+  // let text = 'Kossie';
+
+  const updateText = () => {
+    // text = 'Coder';
+    setText('Coder')
+    console.log(text)
+  }
+
+  console.log(text)
+
   return (
     <div className="App">
       <input onKeyUp={onKeyUp} />
       <button onClick={onSubmit}>Submit</button>
+
+      <br /><br />
+
+      <span>{text}</span>
+      <button onClick={updateText}>Update</button>
     </div>
   );
 }
