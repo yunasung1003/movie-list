57de7cde84da31b742ee27b8d4783e84138c2d34
Kossie Coder <kossie.coder@gmail.com>
2020-07-31 22:31:51 +1000

    05

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [text, setText] = useState('Kossie');
-  const onSubmit = () => {
-    alert('submitted');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
   };
-
-  const onKeyUp = (event) => {
-    if (event.keyCode === 13) {
-      onSubmit();
-    }
-  }
-
-  // let text = 'Kossie';
-
-  const updateText = () => {
-    // text = 'Coder';
-    setText('Coder')
-    console.log(text)
-  }
-
-  console.log(text)
 
   return (
     <div className="App">
-      <input onKeyUp={onKeyUp} />
-      <button onClick={onSubmit}>Submit</button>
-
-      <br /><br />
-
-      <span>{text}</span>
-      <button onClick={updateText}>Update</button>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text"
+          placeholder="Username" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)}
+        /><br />
+        <input 
+          type="password"
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)}
+        /><br />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
