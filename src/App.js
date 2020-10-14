2b9ff22360134fc9925262607878dfc5ae832894
Kossie Coder <kossie.coder@gmail.com>
2020-10-14 23:08:22 +1000

    09

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
-import Counter from './components/Counter';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [buttonName, setButtonName] = useState('클릭');
-  const clickButton = () => {
-    setButtonName('click');
-  };
+  const [condition, setCondition] = useState(false);
+  const toggle = () => setCondition(!condition);
+  useEffect(() => {
+    console.log(condition);
+  }, [condition])
+  const renderCondition = condition 
+    ? 'True'
+    : 'False'
   return (
     <div className="App">
       <h1>Kossie Coder</h1>
-      <Counter click="click1" />
-      <Counter click={buttonName} />
-      <Counter />
-      <button onClick={clickButton}>Click</button>
+      <div>
+        {renderCondition}     
+      </div>
+      <button onClick={toggle}>Toggle</button>
     </div>
   );
 }
