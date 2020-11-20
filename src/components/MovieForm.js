26bb5901f82a38bcf401ec83abc68b7e50796dbd
Kossie Coder <kossie.coder@gmail.com>
2020-11-20 22:15:44 +1000

    16

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -3,20 +3,46 @@ import React, { useState } from 'react';
 const MovieForm = ({ addMovie }) => {
     const [movieTitle, setMovieTitle] = useState('');
     const [movieYear, setMovieYear] = useState('');
+    const [titleError, setTitleError] = useState('');
+    const [yearError, setYearError] = useState('');
   
     const resetForm = () => {
         setMovieTitle('');
         setMovieYear('');
     };
 
+    const validateForm = () => {
+      resetErrors();
+      let validated = true;
+      if (!movieTitle) {
+        setTitleError('영화제목을 넣어주세요');
+        validated = false;
+      }
+
+      if (!movieYear) {
+        setYearError('개봉년도를 넣어주세요');
+        validated = false;
+      }
+
+      return validated;
+    };
+
+    const resetErrors = () => {
+      setTitleError('');
+      setYearError('');
+    };
+
     const onSubmit = (event) => {
       event.preventDefault();
-      addMovie({
-        id: Date.now(),
-        title: movieTitle,
-        year: movieYear,
-      });
-      resetForm();
+      if (validateForm()) {
+        addMovie({
+          id: Date.now(),
+          title: movieTitle,
+          year: movieYear,
+        });
+        resetErrors();
+        resetForm();
+      }
     };
     return (
     <form onSubmit={onSubmit}>
@@ -26,12 +52,14 @@ const MovieForm = ({ addMovie }) => {
         placeholder="영화제목"
         onChange={e => setMovieTitle(e.target.value)}
         /><br />
+        <div style={{color: 'red'}}>{titleError}</div>
         <input 
-        type="text"
+        type="number"
         value={movieYear}
         placeholder="개봉년도"
         onChange={e => setMovieYear(e.target.value)}
         /><br />
+        <div style={{color: 'red'}}>{yearError}</div>
         <button type="submit">영화추가</button>
     </form>
     );
