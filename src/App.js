f58f359a48457625515727d234dfb63fada662a0
Kossie Coder <kossie.coder@gmail.com>
2020-11-29 22:49:12 +1000

    19

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Movie from './components/Movie';
 import MovieForm from './components/MovieForm';
+import Navbar from './components/Navbar';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -28,6 +29,7 @@ function App() {
   };
   return (
     <div className="App">
+      <Navbar />
       <h1>Movie list</h1>
       <MovieForm addMovie={addMovie} />
       {renderMovies}
