b8b795c869af31e296bcd45259ef8910530612fa
Kossie Coder <kossie.coder@gmail.com>
2020-11-30 21:47:01 +1000

    20

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import Movie from './components/Movie';
 import MovieForm from './components/MovieForm';
 import Navbar from './components/Navbar';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 function App() {
   const [movies, setMovies] = useState([]);
 
@@ -28,12 +32,24 @@ function App() {
     ]);
   };
   return (
-    <div className="App">
-      <Navbar />
-      <h1>Movie list</h1>
-      <MovieForm addMovie={addMovie} />
-      {renderMovies}
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/movies">
+            <h1>Movie list</h1>
+            <MovieForm addMovie={addMovie} />
+            {renderMovies}
+          </Route>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Home</a>
+        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+            <Link class="navbar-brand" to="/">Home</Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Movies</a>
+                        <Link class="nav-link" to="/movies">Movies</Link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Users</a>
+                        <Link class="nav-link" to="/users">Users</Link>
                     </li>
                 </ul>
             </div>
