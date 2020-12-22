f259bbdf39232b161f561218c69741b636630f2f
Kossie Coder <kossie.coder@gmail.com>
2020-12-22 11:43:07 +1000

    22

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
-            <Link class="navbar-brand" to="/">Home</Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <button 
+                className="navbar-toggler" 
+                type="button" 
+                data-toggle="collapse" 
+                data-target="#navbarNav" 
+                aria-controls="navbarNav" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <Link class="nav-link" to="/movies">Movies</Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <NavLink 
+                            className="nav-link" 
+                            to="/movies"
+                            activeClassName="active"
+                        >
+                            Movies
+                        </NavLink>
                     </li>
-                    <li class="nav-item">
-                        <Link class="nav-link" to="/users">Users</Link>
+                    <li className="nav-item">
+                        <NavLink 
+                            className="nav-link" 
+                            to="/users"
+                            activeClassName="active"
+                        >
+                            Users
+                        </NavLink>
                     </li>
                 </ul>
             </div>
