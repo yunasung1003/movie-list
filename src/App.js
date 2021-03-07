9dbccff10762b9de76ffe2fb3cbb3696b13989ad
Kossie Coder <kossie.coder@gmail.com>
2021-03-07 21:26:55 +1000

    27

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import React, { useState } from 'react';
-import Movie from './components/Movie';
-import MovieForm from './components/MovieForm';
+import React from 'react';
 import Navbar from './components/Navbar';
 import Users from './pages/Users';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from 'react-router-dom';
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const removeMovie = (id) => {
-    setMovies(movies.filter(movie => {
-      return movie.id !== id;
-    }));
-  };
-
-  const renderMovies = movies.length ? movies.map(movie => {
-    return (
-      <Movie 
-        movie={movie} 
-        key={movie.id}
-        removeMovie={removeMovie}
-      />
-    );
-  }) : '추가된 영화가 없습니다.';
-  const addMovie = (movie) => {
-    setMovies([
-      ...movies,
-      movie
-    ]);
-  };
   return (
     <Router>
       <div className="App">
@@ -39,12 +17,10 @@ function App() {
         <div className="container">
           <Switch>
             <Route path="/movies">
-              <h1>Movie list</h1>
-              <MovieForm addMovie={addMovie} />
-              {renderMovies}
+              <Movies />
             </Route>
             <Route path="/" exact>
-              <h1>Home</h1>
+              <Home />
             </Route>
             <Route path="/users">
               <Users />
