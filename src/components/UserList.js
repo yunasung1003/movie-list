96ab21ce22bb1e7bb8dd3dfb2f901b7c6dff5ad8
Kossie Coder <kossie.coder@gmail.com>
2021-04-06 21:35:29 +1000

    30

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserList = ({ users }) => {
     return (
@@ -10,7 +11,7 @@ const UserList = ({ users }) => {
                         key={user.id}
                     >
                         <div className="card-body p-3">
-                            {user.name}
+                            <Link to={`/users/${user.id}`}>{user.name}</Link>
                         </div>
                     </div>
                 );
--- src/pages/User.js
+++ src/pages/User.js
@@ -8,7 +8,7 @@ const User = () => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const { id } = useParams();
-    console.log(user)
+
     useEffect(() => {
         axios.get('https://jsonplaceholder.typicode.com/users/' + id)
             .then(response => {
@@ -16,12 +16,19 @@ const User = () => {
                 setLoading(false);
             });
     }, []);
+
+    const userDetail = loading ? <Spinner /> : (
+        <div>
+            <div>{user.name}</div>
+            <div>{user.email}</div>
+            <div>{user.phone}</div>
+        </div>
+    )
     
     return (
         <>
             <h1>User 정보</h1>
-            {loading ? <Spinner /> :
-            null}
+            {userDetail}
         </>
     );
 };
