57d6d9aeed843c4edf0bfa896fd31c887651241d
yunasung1003 <yuna.sung.1003@gmail.com>
2021-07-30 00:07:51 +0900

    26

--- src/App.js
+++ src/App.js
@@ -9,29 +9,6 @@ import {
   Route,
 } from 'react-router-dom';
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const removeMovie = (id) => {
-    setMovies(movies.filter(movie => {
-      return movie.id !== id;
-    }));
-  };
-
-  const renderMovies = movies.length ? movies.map(movie => {
-    return (
-      <Movie 
-        movie={movie} 
-        key={movie.id}
-        removeMovie={removeMovie}
-      />
-    );
-  }) : '추가된 영화가 없습니다.';
-  const addMovie = (movie) => {
-    setMovies([
-      ...movies,
-      movie
-    ]);
-  };
   return (
     <Router>
       <div className="App">
